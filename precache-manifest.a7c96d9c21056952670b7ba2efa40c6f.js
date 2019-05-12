785164551b6fa893a583b6f127a32bf28a595e58
Geforceyu <geforceyu@GeforceyudeMacBook-Pro-2.local>
2019-05-12 17:12:20 +0800

    Updates

---
+++ precache-manifest.a7c96d9c21056952670b7ba2efa40c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32182d0779edfaa147f35604353103b0",
+    "url": "/sgstore/index.html"
+  },
+  {
+    "revision": "032e4d59f1ddf16eee3d",
+    "url": "/sgstore/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sgstore/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "032e4d59f1ddf16eee3d",
+    "url": "/sgstore/static/js/main.71399146.chunk.js"
+  },
+  {
+    "revision": "259d055e27b1f333e9aa",
+    "url": "/sgstore/static/js/runtime~main.25bcf49d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sgstore/static/media/logo.5d5d9eef.svg"
+  }
+]);
